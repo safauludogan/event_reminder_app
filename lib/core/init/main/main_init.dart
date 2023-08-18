e244bc045bc308de70e6ca356f1000d8b55d681d
safauludogan <safauludogan@kamutech.com.tr>
2023-08-18 17:28:50 +0300

    Firebase initialized

--- lib/core/init/main/main_init.dart
+++ lib/core/init/main/main_init.dart
@@ -4,7 +4,14 @@ final getIt = GetIt.instance;
 
 Future<void> initInitializes() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await _firebaseInitialize();
   await HiveDatabaseManager().initialize();
   getIt.registerSingleton<AppRouter>(AppRouter());
   await LocaleManager.instance.init();
+}
+
+Future<void> _firebaseInitialize() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
